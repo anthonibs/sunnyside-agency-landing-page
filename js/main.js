5cfc9137501aefd663892047f4b911e9123660e3
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-01-17 23:30:24 -0300

    desafio finalizado

--- js/main.js
+++ js/main.js
@@ -16,3 +16,15 @@ $(window).bind('scroll',function(){
     
     }, espera);
 });
+
+
+//Abrindo e Fechando O MENU MOBILE
+const TOGGLE_MENU = document.querySelectorAll('.btn-menu');
+const OPEN_ANIM_BTN = document.querySelector('.icon');
+const MENU_MOBILE = document.querySelector('.navbar-group-mobile');
+for (let i = 0; i < TOGGLE_MENU.length; i++) {
+  TOGGLE_MENU[i].addEventListener('click', function () {
+      OPEN_ANIM_BTN.classList.toggle('open');
+      MENU_MOBILE.classList.toggle('active');
+  });
+}
