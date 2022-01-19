940f4261427d75455c6942c0b2b0b8586b5263c5
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-01-18 23:40:02 -0300

    resposividade feitas

--- js/main.js
+++ js/main.js
@@ -21,10 +21,12 @@ $(window).bind('scroll',function(){
 //Abrindo e Fechando O MENU MOBILE
 const TOGGLE_MENU = document.querySelectorAll('.btn-menu');
 const OPEN_ANIM_BTN = document.querySelector('.icon');
-const MENU_MOBILE = document.querySelector('.navbar-group-mobile');
+const MENU_MOBILE = document.querySelector('.navbar-container-mobile');
+const OVERLAY_BLUR = document.querySelector('.block-blur-overlay');
 for (let i = 0; i < TOGGLE_MENU.length; i++) {
   TOGGLE_MENU[i].addEventListener('click', function () {
       OPEN_ANIM_BTN.classList.toggle('open');
       MENU_MOBILE.classList.toggle('active');
+      OVERLAY_BLUR.classList.toggle('active');
   });
 }
